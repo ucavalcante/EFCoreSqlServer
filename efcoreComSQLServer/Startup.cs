4392ae5a2910ea4396a36a253f803d9322dbea4e
ulc <ulisses.cavalcante@altitude.com>
2018-12-19 11:54:31 -0200

    add connection with sql server

--- efcoreComSQLServer/Startup.cs
+++ efcoreComSQLServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using efcoreComSQLServer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace efcoreComSQLServer
 {
@@ -33,6 +35,9 @@ namespace efcoreComSQLServer
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = "Server=localhost;Database=teste;User Id=SA;Password=Teste@123;";
+            services.AddDbContext<BloggingContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
